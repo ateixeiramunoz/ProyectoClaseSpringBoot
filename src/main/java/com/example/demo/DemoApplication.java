4d4c17d878c6cde3c776916c63b6c0c049b9faec
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2023-03-14 14:15:19 +0100

    Añado los metodos de mapeo del hola mundo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,30 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String hellow() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/hola")
+	String hola() {
+		return "Hola World!";
+	}
+
+	@RequestMapping("/mimedicacion")
+	String miMedicacion() {
+		return "Tomate las pastillas";
 	}
 
 }
