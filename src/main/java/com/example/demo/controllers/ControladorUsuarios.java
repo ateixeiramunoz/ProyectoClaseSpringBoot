87dcd588f9e2bb0d3b24138902b0e5eb3c43f448
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2023-03-14 21:15:01 +0100

    Primeros cambios de testing

--- src/main/java/com/example/demo/controllers/ControladorUsuarios.java
+++ src/main/java/com/example/demo/controllers/ControladorUsuarios.java
@@ -1,26 +1,25 @@
-package com.example.demo;
-import org.springframework.stereotype.Controller;
+package com.example.demo.controllers;
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@Log4j2
 public class ControladorUsuarios {
-
-    @GetMapping("/")
-    String hellow() {
-        return "Hello World!";
-    }
 
     @GetMapping("/hola")
     String hola() {
-        return "Hola World!";
+        log.warn("Hola Mundo!");
+        System.out.println("Hola Mundo!");
+        return "Hola mundo!";
     }
 
     @GetMapping("/mimedicacion")
     String miMedicacion() {
+        log.warn("Tomate las pastillas");
+        System.out.println("Tomate las pastillas");
         return "Tomate las pastillas";
     }
-
-
 
 }
