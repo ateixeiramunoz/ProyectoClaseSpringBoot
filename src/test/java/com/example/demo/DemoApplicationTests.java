dbd71cca52e7e01b750e64c121839516291488cb
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2023-03-15 02:09:54 +0100

    Incluidos ejemplos de paradigmas POO con ejemplo de muebles

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
@@ -14,5 +13,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @Log4j2
 class DemoApplicationTests {
 
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    void hello_main() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        assertThat(response.getBody()).isEqualTo("Main class");
+    }
+
+    @Test
+    void hello_hola() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/hola", String.class);
+        assertThat(response.getBody()).isEqualTo("Hola mundo!");
+    }
 
 }
